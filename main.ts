8fe617715a49761ee176de2c856b13e4205172f2
Ryan C <rc@ryancash.com>
2022-10-18 12:04:22 -0700

    App prototype with initial functionality hacked in

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as math from `mathjs`;
+
 
 // Remember to rename these classes and interfaces!
 
@@ -16,66 +18,69 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// ** Mathjs Setup ** //
+		const isUnitAlphaOriginal = math.Unit.isValidAlpha;
+		math.Unit.isValidAlpha = function (c, cPrev, cNext) {
+			return isUnitAlphaOriginal(c, cPrev, cNext) || ['$', '€'].includes(c)
+			};	
+		math.createUnit('USD', {aliases:['usd', '$']});
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const preProcessors = [
+			// {regex: /\$((\d|\.|(,\d{3}))+)/g, replace: '$1 USD'}, // Use this if commas haven't been removed already
+			{regex: /,(\d{3})/g, replaceStr: '$1'}, // remove thousands seperators. Will be wrong for add(100,100)
+			{regex: /\$([\d\.]+)/g, replaceStr: '$1 USD'}, // convert $### to ### USD. Assumes commas removed
+		];
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		function numberFormatter(value:number) {
+			return value.toLocaleString();
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		this.registerMarkdownCodeBlockProcessor("math", (source, el, ctx) => {
+			let processedSource = source;
+			for (let processor of preProcessors ) {
+				processedSource = processedSource.replace(processor.regex, processor.replaceStr)
 			}
+			// const preprocessRegex = /(?:\$)([\d,\.]+)/g
+			// const replaceString = '$1 USD' // Delete "$" and append USD to any currency
+			// const processedSource = source.replace(preprocessRegex, replaceString)
+
+			// const rows = processedSource.split("\n").filter((row) => row.length > 0);
+			// const rawRows = source.split("\n").filter((row) => row.length > 0);
+			const rows = processedSource.split("\n");
+			const rawRows = source.split("\n");
+			const results = math.evaluate(rows);
+
+			for (let i = 0; i < rows.length; i++) {
+				const line = el.createEl("div", {cls: "numberpad-line"})
+				line.createEl("span", { text: rawRows[i], cls: "numberpad-input"});
+				// line.createSpan( {text: " → "}, cls: "numberpad-sep");
+				// const formattedResult = math.format(results[i], {upperExp: 7, precision: 8});
+				const emptyLine = (results[i] === undefined)
+				const formattedResult = !emptyLine ? " → " + math.format(results[i], numberFormatter) : '\xa0';
+				line.createEl("span", { text: formattedResult, cls: "numberpad-result" });
+				// console.log(math.parse(rows[i]).toHTML())			  
+			}
+
+
+
+			// // *** Original Working with indepdent lines ***
+			// for (let i = 0; i < rows.length; i++) {
+			// 	const line = el.createEl("div", {cls: "numberpad-line"})
+
+			// 	const result = evaluate(rows[i])
+
+			// 	line.createEl("span", { text: rows[i], cls: "numberpad-input"});
+			// 	// line.createSpan( {text: " → "}, cls: "numberpad-sep");
+			// 	line.createEl("span", { text: " → " + evaluate(rows[i]), cls: "numberpad-result" });			  
+			// }
 		});
+
+		// *********** START Sample Plugin Code *********** //
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// *********** END Sample Plugin Code *********** //
 	}
 
 	onunload() {
