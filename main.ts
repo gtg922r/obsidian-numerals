de53963d9b6680b577c4d1fd3e6addef8c7a84da
Ryan C <rc@ryancash.com>
2022-11-13 01:13:44 -0800

    TeX and Syntax Highlighting, and better error reporting
    - "render" style renamed to Layout style
    - Added a new option for "Render Style" which allows for TeX styling or Syntax Highlighting
    - Added new code block types that force render style: `math-plain`, `math-tex`, `math-highlight`
    - Added a new inline layout style where the result is immediately next to the input
    - Errors now throw the error on the line it occured

--- main.ts
+++ main.ts
@@ -1,94 +1,258 @@
-import { App, DropdownComponent, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, finishRenderMath, Notice, Plugin, PluginSettingTab, renderMath, Setting, loadMathJax} from 'obsidian';
 import * as math from `mathjs`;
 
+enum NumeralsLayout { 
+	TwoPanes,
+	AnswerRight,
+	AnswerBelow,
+	AnswerInline,
+}
+
+const numeralsLayoutClasses = {
+	[NumeralsLayout.TwoPanes]: 		"numerals-panes",
+	[NumeralsLayout.AnswerRight]: 	"numerals-answer-right",
+	[NumeralsLayout.AnswerBelow]: 	"numerals-answer-below",
+	[NumeralsLayout.AnswerInline]: 	"numerals-answer-inline",	
+}
+
+enum NumeralsRenderStyle {
+	Plain = "Plain",
+	TeX ="Tex",
+	SyntaxHighlight = "SyntaxHighlight",
+}
+
+const numeralsRenderStyleClasses = {
+	[NumeralsRenderStyle.Plain]: 			"numerals-plain",
+	[NumeralsRenderStyle.TeX]: 			 	"numerals-tex",
+	[NumeralsRenderStyle.SyntaxHighlight]: 	"numerals-syntax",
+}
+
+// TODO: Add a switch for only rendering input
 
 interface NumeralsSettings {
 	resultSeparator: string;
-	renderStyle: number;
+	layoutStyle: NumeralsLayout;
 	alternateRowColor: boolean;
+	renderStyle: NumeralsRenderStyle;
+}
+
+interface CurrencyType {
+	symbol: string;
+	unicode: string;
+	currency: string;
+	name: string;
 }
 
 const DEFAULT_SETTINGS: NumeralsSettings = {
 	resultSeparator: " → ",
-	renderStyle: 1,
-	alternateRowColor: true
+	layoutStyle: NumeralsLayout.TwoPanes,
+	alternateRowColor: true,
+	renderStyle: NumeralsRenderStyle.Plain,
+}
+
+
+const moneyTypes: CurrencyType[] = [
+	{	symbol: "$", unicode: "x024", currency: "USD", name: "dollar"},
+	{	symbol: "€", unicode: "x20AC", currency: "EUR", name: "euro"},
+	{	symbol: "£", unicode: "x00A3", currency: "GBP", name: "pound"},
+	{	symbol: "¥", unicode: "x00A5", currency: "JPY", name: "yen"},
+];
+
+for (let moneyType of moneyTypes) {
+	math.createUnit(moneyType.currency, {aliases:[moneyType.currency.toLowerCase(), moneyType.symbol]});
+}
+
+// Modify mathjs internal functions to allow for use of currency symbols
+const currencySymbols = moneyTypes.map(m => m.symbol);
+const isAlphaOriginal = math.parse.isAlpha;
+math.parse.isAlpha = function (c, cPrev, cNext) {
+	return isAlphaOriginal(c, cPrev, cNext) || currencySymbols.includes(c)
+	};	
+
+const isUnitAlphaOriginal = math.Unit.isValidAlpha;
+math.Unit.isValidAlpha = function (c, cPrev, cNext) {
+	return isUnitAlphaOriginal(c, cPrev, cNext) || currencySymbols.includes(c)
+	};			
+
+const currencyPreProcessors = moneyTypes.map(m => {
+	return {regex: RegExp('\\' + m.symbol + '([\\d\\.]+)','g'), replaceStr: '$1 ' + m.currency}
+})
+
+const preProcessors = [
+	// {regex: /\$((\d|\.|(,\d{3}))+)/g, replace: '$1 USD'}, // Use this if commas haven't been removed already
+	{regex: /,(\d{3})/g, replaceStr: '$1'}, // remove thousands seperators. Will be wrong for add(100,100)
+	...currencyPreProcessors
+];
+
+function numberFormatter(value:number) {
+	return value.toLocaleString();
+}
+
+function texCurrencyReplacement(input_tex:string) {
+	for (let moneyType of moneyTypes) {
+		input_tex = input_tex.replace(RegExp("\\\\*\\"+moneyType.symbol,'g'),"\\" + moneyType.name);
+	}
+	return input_tex
+}
+
+function htmlToElements(html:string) {
+	var template = document.createElement('template');
+	template.innerHTML = html;
+	const elements = template.content.children;
+	return elements;
+}
+
+async function mathjaxLoop(container: HTMLElement, value: string) {
+	const html = renderMath(value, true);
+	await finishRenderMath()
+
+	// container.empty();
+	container.append(html);
 }
 
 export default class NumeralsPlugin extends Plugin {
 	settings: NumeralsSettings;
 
+	async numeralsMathBlockHandler(type: NumeralsRenderStyle, source: string, el: HTMLElement, ctx): Promise<any> {		
+
+		const blockRenderStyle: NumeralsRenderStyle = type ? type : this.settings.renderStyle;
+		
+		el.toggleClass("numerals-block", true);
+		el.toggleClass(numeralsLayoutClasses[this.settings.layoutStyle], true);
+		el.toggleClass(numeralsRenderStyleClasses[blockRenderStyle], true);			
+		el.toggleClass("numerals-alt-row-color", this.settings.alternateRowColor)
+	
+		let processedSource:string = source;
+		for (let processor of preProcessors ) {
+			processedSource = processedSource.replace(processor.regex, processor.replaceStr)
+		}
+	
+		const rawRows: string[] = source.split("\n");
+		const rows: string[] = processedSource.split("\n");
+		
+		let errorMsg = null;
+		let errorInput = '';
+	
+		let results: string[] = [];
+		let inputs: string[] = [];			
+		let scope = {};
+		
+		try {
+			for (var row of rows.slice(0,-1)) { // Last row is always empty
+				results.push(math.evaluate(row, scope));
+				inputs.push(row); // Only pushes if evaluate is successful
+			}
+		} catch (error) {
+			errorMsg = error;
+			errorInput = row;
+		}	
+						
+		for (let i in inputs) {
+			const line = el.createEl("div", {cls: "numerals-line"});
+			const emptyLine = (results[i] === undefined)
+	
+			switch(blockRenderStyle) {
+				case NumeralsRenderStyle.Plain: {
+					let rawInputSansComment = rawRows[i].replace(/#.+$/, "")
+					let inputText = emptyLine ? rawRows[i] : rawInputSansComment;
+					var inputElement = line.createEl("span", { text: inputText, cls: "numerals-input"});
+					
+					const formattedResult = !emptyLine ? this.settings.resultSeparator + math.format(results[i], numberFormatter) : '\xa0';
+					var resultElement = line.createEl("span", { text: formattedResult, cls: "numerals-result" });
+					break;
+				} case NumeralsRenderStyle.TeX: {
+					let inputText = emptyLine ? rawRows[i] : "";
+					var inputElement = line.createEl("span", {text: inputText, cls: "numerals-input"});
+					const resultContent = !emptyLine ? "" : '\xa0';
+					var resultElement = line.createEl("span", { text: resultContent, cls: "numerals-result" });
+					if (!emptyLine) {
+						// Input to Tex
+						let input_tex:string = math.parse(inputs[i]).toTex();
+						var texElement = inputElement.createEl("span", {cls: "numerals-tex"})
+
+						input_tex = texCurrencyReplacement(input_tex);
+						mathjaxLoop(texElement, input_tex);
+
+						// Result to Tex
+						var texElement = resultElement.createEl("span", {cls: "numerals-tex"})
+						let processedResult:string = math.format(results[i], numberFormatter);
+						for (let processor of preProcessors ) {
+							processedResult = processedResult.replace(processor.regex, processor.replaceStr)
+						}
+						let texResult = math.parse(processedResult).toTex() // TODO: Add custom handler for numbers to get good localeString formatting
+						texResult = texCurrencyReplacement(texResult);
+						mathjaxLoop(texElement, texResult);
+					}
+					break;
+				} case NumeralsRenderStyle.SyntaxHighlight: {
+					let inputText = emptyLine ? rawRows[i] : "";
+					var inputElement = line.createEl("span", {text: inputText, cls: "numerals-input"});
+					if (!emptyLine) {
+						let input_html = htmlToElements(math.parse(inputs[i]).toHTML())
+						for (const element of input_html) {
+							inputElement.createEl("span", {cls: element.className, text: element.innerHTML})
+						}
+					}
+
+					const formattedResult = !emptyLine ? this.settings.resultSeparator + math.format(results[i], numberFormatter) : '\xa0';
+					var resultElement = line.createEl("span", { text: formattedResult, cls: "numerals-result" });
+
+					break;
+				}
+			}
+	
+			if (!emptyLine) {
+				let inlineComment = rawRows[i].match(/#.+$/);
+				if (inlineComment){
+					inputElement.createEl("span", {cls: "numerals-inline-comment", text:inlineComment[0]})
+				}
+			} else {
+				resultElement.toggleClass("numerals-empty", true);
+				inputElement.toggleClass("numerals-empty", true);
+				resultElement.setText('\xa0');
+			}
+		}
+	
+	
+		if (errorMsg) {			
+			const line = el.createEl("div", {cls: "numerals-error-line"});
+			const inputElement = line.createEl("span", { text: errorInput, cls: "numerals-input"});
+			const resultElement = line.createEl("span", {cls: "numerals-result" });
+			resultElement.createEl("span", {cls:"numerals-error-name", text: errorMsg.name + ":"});
+			resultElement.createEl("span", {cls:"numerals-error-message", text: errorMsg.message});		
+		}
+	
+	};
+
+
 	async onload() {
 		await this.loadSettings();
-		
-	
-		// ** Mathjs Setup ** //
-		const isAlphaOriginal = math.parse.isAlpha;
-		math.parse.isAlpha = function (c, cPrev, cNext) {
-			return isAlphaOriginal(c, cPrev, cNext) || ['$', '€', '£', '¥'].includes(c)
-			};	
 
-		const isUnitAlphaOriginal = math.Unit.isValidAlpha;
-		math.Unit.isValidAlpha = function (c, cPrev, cNext) {
-			return isUnitAlphaOriginal(c, cPrev, cNext) || ['$', '€', '£', '¥'].includes(c)
-			};				
-
-		math.createUnit('USD', {aliases:['usd', '$']});
-		math.createUnit('EUR', {aliases:['eur', '€']});
-		math.createUnit('GBP', {aliases:['gbp', '£']});
-		math.createUnit('JPY', {aliases:['jpy', '¥']});
-		
-
-		const preProcessors = [
-			// {regex: /\$((\d|\.|(,\d{3}))+)/g, replace: '$1 USD'}, // Use this if commas haven't been removed already
-			{regex: /,(\d{3})/g, replaceStr: '$1'}, // remove thousands seperators. Will be wrong for add(100,100)
-			{regex: /\$([\d\.]+)/g, replaceStr: '$1 USD'}, // convert $### to ### USD. Assumes commas removed already
-			{regex: /\€([\d\.]+)/g, replaceStr: '$1 EUR'}, // convert €### to ### EUR. Assumes commas removed already
-			{regex: /\£([\d\.]+)/g, replaceStr: '$1 GBP'}, // convert £### to ### GBP. Assumes commas removed already						
-			{regex: /\¥([\d\.]+)/g, replaceStr: '$1 JPY'}, // convert ¥### to ### JPY. Assumes commas removed already									
-		];
-
-		function numberFormatter(value:number) {
-			return value.toLocaleString();
-		}
-
-		this.registerMarkdownCodeBlockProcessor("math", (source, el, ctx) => {
-			el.toggleClass("numerals-panes", 		this.settings.renderStyle == 1)
-			el.toggleClass("numerals-answer-right", this.settings.renderStyle == 2)
-			el.toggleClass("numerals-answer-below", this.settings.renderStyle == 3)						
-
-			el.toggleClass("numerals-alt-row-color", this.settings.alternateRowColor)
-
-			let processedSource = source;
-			for (let processor of preProcessors ) {
-				processedSource = processedSource.replace(processor.regex, processor.replaceStr)
+		// // DEBUGGING PURPOSES ONLY: Add command to reset plugin settings
+		this.addCommand({
+			id: 'reset-numerals-settings',
+			name: 'Reset Numerals Settings to Defaults',
+			callback: async () => {
+					this.settings = DEFAULT_SETTINGS;
+					await this.saveSettings();
+					new Notice('All Numerals settings reset to defaults')					
 			}
+		});		
 
-			const rows = processedSource.split("\n");
-			const rawRows = source.split("\n");
+		// Load MathJax for TeX Rendering
+		await loadMathJax();
 
-			try {
+		// Configure currency commands in MathJax
+		const configureCurrencyStr =  moneyTypes.map(m => '\\def\\' + m.name + '{\\unicode{' + m.unicode + '}}').join('\n');
+		const currencyTex = renderMath(configureCurrencyStr, true);
 
-				const results = math.evaluate(rows);	
-				
-				for (let i = 0; i < rows.length; i++) {
-					const line = el.createEl("div", {cls: "numerals-line"});
-					const inputElement = line.createEl("span", { text: rawRows[i], cls: "numerals-input"});
-	
-					const emptyLine = (results[i] === undefined)
-					const formattedResult = !emptyLine ? this.settings.resultSeparator + math.format(results[i], numberFormatter) : '\xa0';
-					const resultElement = line.createEl("span", { text: formattedResult, cls: "numerals-result" });
-	
-					resultElement.toggleClass("numerals-empty", emptyLine);
-					inputElement.toggleClass("numerals-empty", emptyLine);
-	
-				}				
-
-			} catch (error) {
-				el.createEl("div", {cls: "numerals-error-line"});
-				el.createEl("span", {cls:"numerals-error-name", text: error.name + ":"});
-				el.createEl("span", {cls:"numerals-error-message", text: error.message});		
-			}
-		});
+		
+		this.registerMarkdownCodeBlockProcessor("math", this.numeralsMathBlockHandler.bind(this, null));
+		this.registerMarkdownCodeBlockProcessor("Math", this.numeralsMathBlockHandler.bind(this, null));		
+		this.registerMarkdownCodeBlockProcessor("math-plain", this.numeralsMathBlockHandler.bind(this, NumeralsRenderStyle.Plain));		
+		this.registerMarkdownCodeBlockProcessor("math-tex", this.numeralsMathBlockHandler.bind(this, NumeralsRenderStyle.TeX));
+		this.registerMarkdownCodeBlockProcessor("math-TeX", this.numeralsMathBlockHandler.bind(this, NumeralsRenderStyle.TeX));
+		this.registerMarkdownCodeBlockProcessor("math-highlight", this.numeralsMathBlockHandler.bind(this, NumeralsRenderStyle.SyntaxHighlight));
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new NumeralsSettingTab(this.app, this));
@@ -126,18 +290,33 @@ class NumeralsSettingTab extends PluginSettingTab {
 		containerEl.createEl('h2', {text: 'Settings for my *Numerals* plugin.'});
 
 		new Setting(containerEl)
-			.setName('Numerals Render Style')
-			.setDesc('Style of results rendering in Live Preview and Reading mode')
+			.setName('Numerals Layout Style')
+			.setDesc('Layout of math blocks in Live Preview and Reading mode')
 			.addDropdown(dropDown => {
-				dropDown.addOption(1, '2 Panes');
-				dropDown.addOption(2, 'Answer to the right');
-				dropDown.addOption(3, 'Answer below each line');
+				dropDown.addOption(NumeralsLayout.TwoPanes, '2 Panes');
+				dropDown.addOption(NumeralsLayout.AnswerRight, 'Answer to the right');
+				dropDown.addOption(NumeralsLayout.AnswerBelow, 'Answer below each line');
+				dropDown.addOption(NumeralsLayout.AnswerInline, 'Answer inline, beside input');				
+				dropDown.setValue(this.plugin.settings.layoutStyle);
+				dropDown.onChange(async (value) => {
+					this.plugin.settings.layoutStyle = value;
+					await this.plugin.saveSettings();
+				});
+			});		
+
+		new Setting(containerEl)
+			.setName('Default Numerals Rendering Style')
+			.setDesc('Choose how the input and results are rendered by default. Note that you can specify the rendering style on a per block basis, by using \`math-plain\`, \`math-tex\`, or \`math-highlight\`')
+			.addDropdown(dropDown => {
+				dropDown.addOption(NumeralsRenderStyle.Plain, 'Plain Text');
+				dropDown.addOption(NumeralsRenderStyle.TeX, 'TeX Style');
+				dropDown.addOption(NumeralsRenderStyle.SyntaxHighlight, 'Syntax Highlighting of Plain Text');
 				dropDown.setValue(this.plugin.settings.renderStyle);
 				dropDown.onChange(async (value) => {
 					this.plugin.settings.renderStyle = value;
 					await this.plugin.saveSettings();
 				});
-			});		
+			});				
 
 		new Setting(containerEl)
 			.setName('Result Indicator')
